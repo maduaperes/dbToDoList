0dc1b0ba3dcf7b148edf7f994b40c72974bcadc2
Maria Eduarda <mariaeduardaaperes@hotmail.com>
2025-07-01 20:30:13 -0300

    Update

--- controllers/tarefaController.js
+++ controllers/tarefaController.js
@@ -8,4 +8,41 @@ async function listarTarefas(req,res){
     res.status(500).json({erro: 'Erro ao buscar tarefas'});
   }
 }
-module.exports = {listarTarefas}
+
+async function criarTarefa(req, res) {
+  try {
+    const { descricao } = req.body;
+    await tarefasServices.criarQuerry(descricao);
+    res.status(201).json({ message: 'Tarefa criada com sucesso' });
+  } catch (erro) {
+    res.status(500).json({ erro: 'Erro ao criar tarefa' });
+  }
+}
+
+async function atualizarTarefa(req, res) {
+  try {
+    const { id } = req.params;
+    const { descricao } = req.body;
+    await tarefasServices.atualizarQuerry(id, descricao);
+    res.status(200).json({ message: 'Tarefa atualizada com sucesso' });
+  } catch (erro) {
+    res.status(500).json({ erro: 'Erro ao atualizar tarefa' });
+  }
+}
+
+async function deletarTarefa(req, res) {
+  try {
+    const { id } = req.params;
+    await tarefasServices.deletarQuerry(id);
+    res.status(200).json({ message: 'Tarefa deletada com sucesso' });
+  } catch (erro) {
+    res.status(500).json({ erro: 'Erro ao deletar tarefa' });
+  }
+}
+
+module.exports = {
+  listarTarefas,
+  criarTarefa,
+  atualizarTarefa,
+  deletarTarefa
+};
--- services/tarefasServices.js
+++ services/tarefasServices.js
@@ -1,11 +1,28 @@
 const db = require("../config/db.js");
 
 async function listarQuerry(){
-   const [rows] = await db.query("SELECT * FROM tarefas");
-   return rows;
+   const [resultado] = await db.query('SELECT * FROM tarefas');
+   return resultado;
 }
- module.exports = {
-    listarQuerry
+async function criarQuerry({titulo, descricao, status, prioridade, data_entrega}) {
+   const query = 'INSERT INTO tarefas (titulo, descricao, status, prioridade, data_entrega) ' + 'VALUES (?, ?, ?, ?, ?)';
+   await db.query(query, [titulo, descricao, status, prioridade, data_entrega]);
 }
 
- 
+async function atualizarQuerry(id, {titulo, descricao, status, prioridade, data_entrega}) {
+   const query = 'UPDATE tarefas SET titulo = ?, descricao = ?, status = ?, prioridade = ?, data_entrega = ? ' + ' WHERE id = ?';
+   const [resultado] = await db.query(query, [titulo, descricao, status, prioridade, data_entrega, id]);
+   return resultado;
+}
+
+async function deletarQuerry(id) {
+   const query = 'DELETE FROM tarefas WHERE id = ?';
+   const [resultado] = await db.query(query, [id]);
+   return resultado.affectedRows;
+}
+ module.exports = {
+    listarQuerry,
+    criarQuerry,
+    atualizarQuerry,
+    deletarQuerry
+}
